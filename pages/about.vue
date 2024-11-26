4ce8bc8ac68fd7eafda6b3332c242cf648578e78
Chandler Hardy <hardych04@gmail.com>
2024-11-26 13:46:31 -0600

    Added index and about pages

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>About Us</h1>
+        <p>This is the about page.</p>
+        <nuxt-link to="/">Go back to Home</nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AboutPage'
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: #2c3e50;
+    }
+</style>
